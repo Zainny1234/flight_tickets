24c898da10b3076c0e7c8fed4bd02317469d0d45
zainny1234 <zainny151@gmail.com>
2020-02-28 18:19:45 +0530

    preprocess class

--- src/final preprocess.py
+++ src/final preprocess.py
@@ -3,16 +3,16 @@ import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-
+import os
+import joblib
 from src.data_ingest import load_dataset
 from conf.config import DAY_OF_BOOKING, CATEGORICAL_COLUMNS
 from src.util import *
 
 
-class Preprocess:
-    def __int__(self, X, Y):
+class PreProcess:
+    def __init__(self, X):
         self.x = X
-        self.y = Y
 
     def basefeat(self, ms, bk_class):
         self.x = marketshare(self.x, ms)
@@ -21,26 +21,45 @@ class Preprocess:
         self.x = timeofday(self.x)
         self.x = totalstops(self.x)
         self.x = X(self.x)
-        return self
+        return self.x
 
-    def vectoriser(self, col, training=True):
-        self.x[col] = self.x[col].apply(clean_route)
+    def vectoriser(self, training=True):
+        self.x['Route'] = self.x['Route'].apply(clean_route)
         if training:
             tf = TfidfVectorizer(ngram_range=(1, 1), lowercase=False)
-            route = tf.fit_transform(self.x[col])
+            route = tf.fit_transform(self.x['Route'])
+            joblib.dump(tf, os.path.join(os.getcwd(), 'models', 'tf.sav'))
         else:
             #load tf fomr json(write code for it)
-            route = tf.fit_transform(self.x['col'])
+            tf = joblib.load(os.path.join(os.getcwd(), 'models', 'tf.sav'))
+            route = tf.fit_transform(self.x['Route'])
 
+        route = pd.DataFrame(data=route.toarray(), columns=tf.get_feature_names())
         self.x = pd.concat([self.x, route], axis=1)
         self.x.drop('Route', axis=1, inplace=True)
-        return self
+        return self.x
 
-    def create_dummies(self, cols):
-        self.x = pd.get_dummies(self.x, cols)
+    def create_dummies(self, cat_cols):
+        self.x = pd.get_dummies(self.x, cat_cols)
+        return self.x
+
+    def preprocess(self, ms, bk_class,  cat_cols, training = True):
+        self.x = self.basefeat(ms, bk_class)
+        self.x = self.vectoriser(training)
+        self.x = self.create_dummies(cat_cols)
+        return self.x
 
 
+if __name__ == "__main__":
+    from src.data_ingest import load_dataset
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    from conf.config import  CATEGORICAL_COLUMNS as cat_cols
+    x = load_dataset('train.xlsx')
+    ms = load_dataset('ms.json')['market']
+    bk_class = load_dataset('class.json')['class']
 
+    p = PreProcess(x)
+    data = p.preprocess(ms, bk_class, cat_cols, training = False)
 
 
 
